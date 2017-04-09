fe4461e1cd804ba0e38e7f4e4ec2d3ba6fb8469d
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-04-09 16:25:28 -0700

    array.c showing how to use arrays

---
+++ array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int arr[3];
+
+  arr[0] = 100;
+  arr[1] = 200;
+  arr[2] = 300;
+  char str[5] = {'T','e','s','t','\0'};
+
+  printf("1st element value: \t %d \n", arr[0]);
+  printf("2nd element value: \t %d \n", arr[1]);
+  printf("3rd element value: \t %d \n", arr[2]);
+  printf("String: \t \t %s", str)  ;
+
+  return 0;
+}
